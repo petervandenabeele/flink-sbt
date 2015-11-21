7934e410c45343505ee1e20e490d6184f59b3fb6
Peter Vandenabeele <peter@vandenabeele.com>
2015-11-21 21:09:43 +0100

    A minimal WordCount on Apache Flink 0.10.0

---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,23 @@
+package io.allthingsdata.wordCount
+
+import org.apache.flink.api.scala._
+
+object WordCount {
+  def main(args: Array[String]) {
+
+    // set up the execution environment
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    // get input data
+    val text = env.fromElements("To be, or not to be,--that is the question:--")
+
+    val counts = text.flatMap { _.toLowerCase.split("\\W+") }
+      .map { (_, 1) }
+      .groupBy(0)
+      .sum(1)
+
+    // execute and print result
+    counts.print()
+
+  }
+}
